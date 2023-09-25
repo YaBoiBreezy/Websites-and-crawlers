583daee00e89879882646fd209788e4d08a4fe26
benjamincyiza <95043117+benjamincyiza@users.noreply.github.com>
2023-09-25 16:33:56 -0400

    crawler

--- lab3/prisma/seed.js
+++ lab3/prisma/seed.js
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-import * as data from "./data.js";
 
 let prisma = new PrismaClient();
 
--- lab3/src/crawler.js
+++ lab3/src/crawler.js
@@ -1,36 +1,74 @@
 //Required module (install via NPM - npm install crawler)
 import Crawler from "crawler";
+import { PrismaClient } from "@prisma/client";
 
-// Now you can use the Crawler module as you normally would
 
+// database
+let db = new PrismaClient();
 
 const c = new Crawler({
-    maxConnections : 10, //use this for parallel, rateLimit for individual
-    //rateLimit: 1000,
+  maxConnections: 10, //use this for parallel, rateLimit for individual
+  //rateLimit: 1000,
 
-    // This will be called for each crawled page
-    callback : function (error, res, done) {
-        if(error){
-            console.log(error);
-        }else{
-            let $ = res.$; //get cheerio data, see cheerio docs for info
-            let links = $("a") //get all links from page
-            $(links).each(function(i, link){
-              //Log out links
-              //In real crawler, do processing, decide if they need to be added to queue
-              console.log($(link).text() + ':  ' + $(link).attr('href'));
-            });
+  // This will be called for each crawled page
+  callback: function (error, res, done) {
+    if (error) {
+      console.log(error);
+    } else {
+      let $ = res.$; //get cheerio data, see cheerio docs for info
+      let links = $("a"); //get all links from page
+      const url = res.request.uri.href;
+
+      $(links).each(async function (i, link) {
+        const absoluteLink = new URL($(link).attr("href"), url).href;
+
+        try {
+          // Use the Prisma client to find a page record by its URL
+          const page = await db.page.findUnique({
+            where: {
+              url: absoluteLink,
+            },
+            select: {
+              url: true,
+              visitedAt: true,
+            },
+          });
+
+          if (!page) {
+            console.log(page.url );
+
+            // console.log('\t'+$(link).text() + ':  ' + $(link).attr('href'));
+
+            //creating new page record
+            try {
+                const newPage = db.page.create({
+                data: {
+                    url: absoluteLink,
+                },
+                });
+
+                console.log("New Page Created:", newPage.url);
+            } catch (error) {
+                console.error("Error adding page record:", error);
+            }
+            c.queue(absoluteLink);
+          } 
+        } catch (error) {
+          console.error("Error retrieving page record:", error);
         }
-        done();
+
+      });
     }
+    done();
+  },
 });
 
 //Perhaps a useful event
 //Triggered when the queue becomes empty
 //There are some other events, check crawler docs
-c.on('drain',function(){
-    console.log("Done.");
+c.on("drain", function () {
+  console.log("Done.");
 });
 
 //Queue a URL, which starts the crawl
-c.queue('https://people.scs.carleton.ca/~davidmckenney/fruitgraph/N-0.html');
+c.queue("https://people.scs.carleton.ca/~davidmckenney/fruitgraph/N-0.html");
