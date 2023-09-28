4551086f2611901428eb9db9a9837a14832e7b71
benjamincyiza <95043117+benjamincyiza@users.noreply.github.com>
2023-09-27 22:08:21 -0400

    .

--- lab3/src/crawler.js
+++ lab3/src/crawler.js
@@ -1,5 +1,9 @@
 //Required module (install via NPM - npm install crawler)
 import Crawler from "crawler";
+import { PrismaClient } from "@prisma/client";
+
+// database
+let db = new PrismaClient();
 
 let visite= new Set();
 const c = new Crawler({
@@ -7,26 +11,67 @@ const c = new Crawler({
   //rateLimit: 1000,
 
   // This will be called for each crawled page
-  callback: function (error, res, done) {
+  callback: async function (error, res, done) {
     if (error) {
       console.log(error);
     } else {
       let $ = res.$; //get cheerio data, see cheerio docs for info
       let links = $("a"); //get all links from page
       const url = res.request.uri.href;
-      visite.add(url)
-      console.log(url);
-      $(links).each(function (i, link) {
+
+      let page= await db.page.findUnique({
+        where: {
+          url: url
+        },
+        select: {
+          id: true,
+          url: true,
+
+        }
+      })
+
+      if(!page){
+        page= await db.page.create({ data : {url:url}})   
+      }   
+      console.log( page.id +": "+page.url );
+
+
+      $(links).each(async function (i, link) {
         //Log out links
         //In real crawler, do processing, decide if they need to be added to queue
         //   console.log($(link).text() + ':  ' + $(link).attr('href'));
         const absoluteLink = new URL($(link).attr("href"), url).href;
-        if(!visite.has(absoluteLink)){
-          console.log("\t" + absoluteLink);
-          c.queue(absoluteLink);
+
+        let newPage = await db.page.findUnique({
+          where: {
+            url: absoluteLink
+          },
+          select: {
+            id: true,
+            url: true,
+            outgoing: true
+  
+          }
+        })
+
+        if(!newPage){
+          newPage= await db.page.create({ data : {url:absoluteLink}});
+        }  
+
+        console.log( "\t"+newPage.id +": "+newPage.url );
+
+        db.page.update({
+          where: {id: page.id},
+          data: { outgoing: newPage.id}
+        })
+
+        if(!newPage.outgoing){
+          c.queue(newPage.url);
         }
+        
       });
     }
+
     done();
   },
 });
@@ -35,8 +80,23 @@ const c = new Crawler({
 //Triggered when the queue becomes empty
 //There are some other events, check crawler docs
 c.on("drain", function () {
+  
   console.log("Done.");
 });
 
 //Queue a URL, which starts the crawl
 c.queue("https://people.scs.carleton.ca/~davidmckenney/fruitgraph/N-0.html");
+
+
+async function findPage(url) {
+   return await db.page.findUnique({
+    where: {
+      url: url
+    },
+    select: {
+      id: true,
+      url: true,
+
+    }
+  })
+}
