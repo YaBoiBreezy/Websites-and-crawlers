e688d497f75d640f3765f8ece6cb8d15a820a89e
benjamincyiza <95043117+benjamincyiza@users.noreply.github.com>
2023-11-06 11:22:13 -0500

    similarity

--- lab6/COllaborative.py
+++ lab6/COllaborative.py
@@ -1,4 +1,31 @@
-file_path = "C:\\Users\\cyiza\\Documents\\GitHub\\COMP4601\\lab6\\test3.txt"
+from math import sqrt
+
+file_path = "C:\\Users\\cyiza\\Documents\\GitHub\\COMP4601\\lab6\\test.txt"
+
+#calculating rate average for a user
+def avg(user):
+    print(user)
+    #new array by excluding negative numbers
+    userRating= [num for num in user if int(num) >= 0]
+    average = sum(userRating) / len(userRating) if len(userRating) > 0 else 0
+    return average
+
+#calculating similarity
+def sim(userA, userB):
+    avgA=avg(userA)
+    avgB=avg(userB)
+
+    numerator=0
+    denA=0
+    denB=0
+    for i in range(numItems):
+        if userA[i]<0 or userB[i]<0:
+            continue
+        numerator+=(userA[i]-avgA)*(userB[i] - avgB)
+        denA+=(userA[i]-avgA)**2
+        denB+=(userB[i] - avgB)**2
+    similarity=numerator/(sqrt(denA)*sqrt(denB))
+    return similarity
 
 #number of users and items
 numUsers=0
@@ -13,17 +40,26 @@ try:
         if lines:
             numUSerAndItems=lines[0].split()
 
-            numUsers=numUSerAndItems[0]
-            numItems=numUSerAndItems[1]
+            numUsers=int( numUSerAndItems[0])
+            numItems=int( numUSerAndItems[1])
 
             users=lines[1].split()
             items=lines[2].split()
 
-            print(users[0])
-            print(items[0])
-        for line in lines:
-            print(line.strip())  # strip() removes leading and trailing whitespaces
+            rating=[]
+        for line in lines[3:]:
+            # Split the line into individual numbers and convert them to integers
+            row = [int(num) for num in line.split()]
+            
+            # Append the row to the two-dimensional array
+            rating.append(row)
+        print(sim(rating[0],rating[4]))
+
 except FileNotFoundError:
     print(f"The file at '{file_path}' does not exist.")
 except Exception as e:
     print(f"An error occurred: {e}")
+
+
+
+
